105c8414865c356003ca2f9cf6a4399ac28c7621
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-09-23 13:21:29 +0530

    updated Designs

--- src/Components/TodoApp/TodoApp.jsx
+++ src/Components/TodoApp/TodoApp.jsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import "./TodoApp.css"
+import React, { Component } from "react";
+import "./TodoApp.css";
 export default class TodoApp extends Component {
-
   render() {
     return (
-      <div className='todo-container'>
-        <h1>Todo App</h1>
-
-        <div className='input-section'>
-          <input type="text" />
-          <button>Add</button>
-        </div>
+      <div className="todo-container">
+        <form className="input-section">
+          <h1>Todo App</h1>
+          <input type="text" placeholder="Enter Items..." />
+        </form>
 
         <ul>
-          <li>
-            items
-          </li>
+          <li>items</li>
+          <li>items</li>
+          <li>items</li>
+          <li>items</li>
         </ul>
       </div>
-    )
+    );
   }
 }

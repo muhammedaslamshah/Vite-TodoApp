d104b43dde9a12a7faa98e2af8384d07999c07d7
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-09-23 14:17:51 +0530

    Todo adding function Updated

--- src/Components/TodoApp/TodoApp.jsx
+++ src/Components/TodoApp/TodoApp.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
 import "./TodoApp.css";
 export default class TodoApp extends Component {
+  state = {
+    input: "",
+    items: [],
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+
+  storeItems = (event) => {
+    event.preventDefault();
+    const { input } = this.state;
+
+    this.setState({
+      items: [...this.state.items, input],
+    });
+  };
+
   render() {
+    const { input, items } = this.state;
+    console.log(items);
+
     return (
-      <div className="todo-container">
+      <div className="todo-container" onSubmit={this.storeItems}>
         <form className="input-section">
           <h1>Todo App</h1>
-          <input type="text" placeholder="Enter Items..." />
+          <input
+            type="text"
+            value={input}
+            onChange={this.handleChange}
+            placeholder="Enter Items..."
+          />
         </form>
 
         <ul>
-          <li>items <i className="fas fa-trash-alt"></i></li>
-          <li>items <i className="fas fa-trash-alt"></i></li>
-          <li>items <i className="fas fa-trash-alt"></i></li>
+          {items.map((data, index) => (
+            <li key={index}>
+              {data}
+              <i className="fas fa-trash-alt"></i>
+            </li>
+          ))}
         </ul>
       </div>
     );
